c770238c7c36847f43210a961b95c3a7eebe17ab
Magic0913 <magicjohnson.ca@gmail.com>
2022-06-20 22:26:39 +0900

    Complete

---
+++ readme.md
@@ -0,0 +1,20 @@
+Introduction
+  This module is for changing all files of folder you selected depending on your changing requirement about file contents and file name.
+
+1. Your files must be copy in "complete" folder
+2. Set up your changing requirement in function generateImageName or customize func "generateImageName" depending on your requirement. 
+
+  const newName = "Chameleons_Piece_" + addName + oldName;
+
+3. Get your changing name from func "generateImageName" and then change value "replaceData" in func "changeContent"
+
+  const genName = generateImageName(
+    name.replace("piece_", "").replace(".json", "")
+  );
+
+  const replaceDate = data.replace(
+    /"image": null/g,
+    `"image": "${genName}.png"`
+  );
+
+4. And then run "node updateFilecontents.js" in command line
---
+++ updateFilecontents.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+const { getEnabledCategories } = require("trace_events");
+
+function generateImageName(oldName) {
+  let addName = "0";
+  for (i = 5 - oldName.length; i > 1; i--) {
+    addName += "0";
+  }
+  const newName = "Chameleons_Piece_" + addName + oldName;
+  return newName;
+}
+
+function readFiles(dir, processFile) {
+  // read directory
+  fs.readdir(dir, (error, fileNames) => {
+    if (error) throw error;
+
+    fileNames.forEach((filename) => {
+      const fullpath = path.resolve(dir, filename);
+      processFile(fs, fullpath, filename);
+    });
+  });
+}
+
+function changeContent(fs, fullpath, filename) {
+  // changing content
+  let name = filename;
+  const genName = generateImageName(
+    name.replace("piece_", "").replace(".json", "")
+  );
+
+  const data = fs.readFileSync(fullpath, "utf8");
+  const replaceDate = data.replace(
+    /"image": null/g,
+    `"image": "${genName}.png"`
+  );
+
+  fs.writeFileSync(fullpath, replaceDate);
+}
+
+function changeFileName(fs, fullpath) {
+  // final step: rename file
+  let rename = fullpath.replace(/piece_/g, "").replace(".json", "");
+  fs.rename(fullpath, rename, function (err) {
+    if (err) throw err;
+  });
+}
+
+readFiles("./complete", changeContent);
+readFiles("./complete", changeFileName);
